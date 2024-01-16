6005fe2b921286304200cdd9e4ff73def2ca3a55
Jose Salazar <jsalazar@pneumarespiratory.com>
2024-01-16 00:16:21 -0500

    Used a CNN using Pytorch to classify handwritten digits from MNIST dataset. Used a set of labled images and then checked performance by calculating accuracy with a seperate test set

--- TrainCNN.py
+++ TrainCNN.py
@@ -4,4 +4,66 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.utils.data import DataLoader
 
+# Define the Net class
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+# Transform, datasets, and dataloaders
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5,), (0.5,))]
+)
+trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+train_loader = DataLoader(trainset, batch_size=64, shuffle=True)
+test_loader = DataLoader(testset, batch_size=64, shuffle=False)
+
+# Initialize the network, loss function, and optimizer
+net = Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters())
+
+# Training loop
+for i, data in enumerate(train_loader, 0):
+    inputs, labels = data
+    optimizer.zero_grad()
+    outputs = net(inputs)
+    loss = criterion(outputs, labels)
+    loss.backward()
+    optimizer.step()
+
+# Testing loop
+net.eval()
+correct = 0
+total = 0
+for data in test_loader:
+    images, labels = data
+    outputs = net(images)
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the test images: %d %%' % (100 * correct / total))
